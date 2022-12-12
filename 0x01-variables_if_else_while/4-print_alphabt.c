13d5ef9f47a8c8ed7625f0a742de5c7bca728e16
ParadiseLtd <mailstouzo@gmail.com>
2022-12-12 08:21:44 -0800

    4-print_alphabt.c

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -9,19 +9,14 @@ int main(void)
 {
 	char c;
 
-	char d;
-
 	c = 'a';
-	c = 'A';
+
 	while
 		(c <= 'z') {
-			putchar(c);
+			if ((c != 'q' && c != 'e') && c <='z')
+				putchar(c);
 			c++;
 		}
-	while
-		(d <= 'z') {
-			putchar(d);
-			d++;
-		}
+	putchar('\n');
 	return (0);
 }
