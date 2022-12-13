1479f45198cb78b74dbf525763cdeb0d3cc8262b
ParadiseLtd <mailstouzo@gmail.com>
2022-12-13 10:20:38 -0800

    1-alphabet.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -2,15 +2,13 @@
 
 /**
  * print_alphabet - print the lowercase alphabets
- *
- * Return: Always 0 (success)
  */
 
 void print_alphabet(void)
 {
 	int j;
 
-	for (c = 'a'; j <= 'z'; j++)
+	for (j = 'a'; j <= 'z'; j++)
 	{
 		_putchar(j);
 	}
